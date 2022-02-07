b74883d5325b3d2d079fba2ca84fbcb03ebd204a
Ahmad Ibrahim <ahmadaburabee3@gmail.com>
2022-02-07 10:53:34 -0700

    Removed commented code

--- app.js
+++ app.js
@@ -27,26 +27,10 @@ async function testConnection() {
     console.error("Unable to connect to the database:", error);
   }
 }
+// testConnection(); // run this function to test database connection
 
-// testConnection();
 sequelize.sync();
 
 app.use("/", indexRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render("error", { err });
-// });
 
 module.exports = app;
